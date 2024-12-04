b45b751722d0640f834124ff0268c2b6a326214c
marpez <ing.alfonso.marpez@gmail.com>
2024-12-04 09:38:56 -0600

    Initial Commit

---
+++ appNutrilogiaIOS/appNutrilogiaIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  appNutrilogiaIOSApp.swift
+//  appNutrilogiaIOS
+//
+//  Created by Alfonso Martinez on 04/12/24.
+//
+
+import SwiftUI
+
+@main
+struct appNutrilogiaIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
